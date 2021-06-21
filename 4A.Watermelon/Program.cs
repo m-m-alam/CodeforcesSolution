c50adad0bfd39467beb2ec5276f495da585aea76
m-alam <m.m.alam6bs@gmail.com>
2021-06-21 23:48:20 +0600

    Add Watermelon problem solution

---
+++ 4A.Watermelon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4A.Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if(x > 2 && x % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
