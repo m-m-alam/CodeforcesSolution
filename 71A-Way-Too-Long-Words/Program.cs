68c12ab87bf5da0348ebbf25d9a79177ad89c6e5
m-alam <m.m.alam6bs@gmail.com>
2021-06-30 01:32:48 +0600

    71A-Way-Too-Long-Words solution

---
+++ 71A-Way-Too-Long-Words/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _71A_Way_Too_Long_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                var word = Console.ReadLine();
+                if (word.Length > 10)
+                {
+                    var result = word[0] + (word.Length - 2).ToString() + word[word.Length - 1];                    
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine(word);
+                }
+                n--;
+            }
+        }
+    }
+}
