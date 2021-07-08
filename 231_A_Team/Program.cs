3c79cad49d97da8bc1d42392a9327cb971f28cc6
m-alam <m.m.alam6bs@gmail.com>
2021-07-08 22:24:02 +0600

    solution of 231_A_Team

---
+++ 231_A_Team/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _231_A_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Petya, Vasya, Tonya, result=0;
+            int problem = Int32.Parse(Console.ReadLine());
+            for(int i=0; i < problem; i++)
+            {
+                var input = Console.ReadLine().Split(" ");
+                Petya = Convert.ToInt32(input[0]);
+                Vasya = Convert.ToInt32(input[1]);
+                Tonya = Convert.ToInt32(input[2]);
+                if (Petya + Vasya + Tonya >= 2)
+                {
+                    result++;
+                }
+
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
