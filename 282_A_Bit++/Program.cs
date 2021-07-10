4cafcb404ea2732a0862d545e43f75e976d88510
m-alam <m.m.alam6bs@gmail.com>
2021-07-11 00:05:44 +0600

    solution of 282_A_Bit++

---
+++ 282_A_Bit++/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _282_A_Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int n = Int16.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var statement = Console.ReadLine();
+                if (statement == "++X" || statement == "X++")
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+            }
+            Console.WriteLine(x);
+            
+        }
+    }
+}
