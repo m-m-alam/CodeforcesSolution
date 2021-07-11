7dacd607d86c0d64b3bb10a2f8ba25a3f60cd53b
m-alam <m.m.alam6bs@gmail.com>
2021-07-11 23:52:25 +0600

    solution of 112_A_Petya and Strings

---
+++ 112_A_Petya and Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _112_A_Petya_and_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firsString = Console.ReadLine();
+            var secondString = Console.ReadLine();
+            //if (firsString == secondString)
+            //{
+            //    Console.WriteLine("0");
+            //}
+            //else if (firsString < secondString)
+            //{
+            //    Console.WriteLine("0");
+            //}
+            var result = string.Compare(firsString.ToLower(), secondString.ToLower());
+            Console.WriteLine(result);
+        }
+    }
+}
