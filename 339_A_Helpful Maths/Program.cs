4362c26c3e069df5bb0f04b815e572a9104c3e3f
Mahabub <m.m.alam6bs@gmail.commahabub@vonome.com>
2021-12-28 22:31:35 +0600

    add helpfull math code

---
+++ 339_A_Helpful Maths/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _339_A_Helpful_Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var math = Console.ReadLine().Split("+");            
+             Array.Sort(math);
+            var result = string.Join("+", math);
+            Console.WriteLine(result);
+        }
+    }
+}
